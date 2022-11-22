ab331e7c5a0a66176e05a1ac673cb3116d57c7d9
Manogel <manoelgomes53@gmail.com>
2022-11-21 22:49:05 -0300

    feat: update scripts

--- src/stages/fetchBranch.ts
+++ src/stages/fetchBranch.ts
@@ -2,7 +2,7 @@ import { exec } from '@actions/exec';
 
 export const fetchBranch = async (targetBranch: string) => {
   try {
-    await exec(`git fetch --no-tags --depth=1 origin '${targetBranch}'`);
+    await exec(`git fetch --no-tags --depth=1 origin ${targetBranch}`);
   } catch (err) {
     console.warn(`Error to fetch ${targetBranch} branch`, err);
     throw err;
