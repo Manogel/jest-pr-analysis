dd33d80525827cc0c13729eb96b32bf3a8d73f52
Manogel <manoelgomes53@gmail.com>
2022-12-05 23:20:49 -0300

    fix: mock test

--- tests/src/stages/getRelatedTestFiles.spec.ts
+++ tests/src/stages/getRelatedTestFiles.spec.ts
@@ -1,11 +1,11 @@
-import exec from '@actions/exec';
+import { getExecOutput } from '@actions/exec';
 import { getRelatedTestFiles } from '~/stages/getRelatedTestFiles';
 
 describe('getRelatedTestFiles', () => {
   const filesArray = ['path-to-file'];
   it('should be execute jest command by parameter', async () => {
     await getRelatedTestFiles(filesArray);
-    expect(exec.getExecOutput).toBeCalledWith(
+    expect(getExecOutput).toBeCalledWith(
       `yarn jest --listTests --findRelatedTests ${filesArray.join(' ')}`,
     );
   });
