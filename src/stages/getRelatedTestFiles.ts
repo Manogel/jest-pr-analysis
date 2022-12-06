2214e358cb1d6a7eabf610b8f263de902a9f60b7
Manogel <manoelgomes53@gmail.com>
2022-12-05 22:25:52 -0300

    debug: add logs

--- src/stages/getRelatedTestFiles.ts
+++ src/stages/getRelatedTestFiles.ts
@@ -2,9 +2,10 @@ import { getExecOutput } from '@actions/exec';
 
 export const getRelatedTestFiles = async (changedFilesArray: string[]) => {
   try {
-    const { stdout } = await getExecOutput(
+    const { stdout, ...rest } = await getExecOutput(
       `yarn jest --listTests --findRelatedTests ${changedFilesArray.join(' ')}`,
     );
+    console.log(rest);
     return stdout;
   } catch (err) {
     console.warn('Error to show files diff', err);
