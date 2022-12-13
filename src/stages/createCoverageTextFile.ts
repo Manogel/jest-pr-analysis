15168cea3d9b954361cad48c207ab0c0aec7ea56
Manogel <manoelgomes53@gmail.com>
2022-12-13 19:41:25 -0300

    feat: create fn createCoverageTextFile

---
+++ src/stages/createCoverageTextFile.ts
@@ -0,0 +1,20 @@
+import { info } from '@actions/core';
+import fs from 'fs-extra';
+
+export const createCoverageTextFile = (filePath: string) => {
+  if (fs.existsSync(filePath)) {
+    info(`path ${filePath} exists`);
+    return;
+  }
+
+  const paths = filePath.split('/').filter((pathItem) => pathItem !== '.');
+
+  paths.pop(); // drop filename
+  const formattedPath = paths.join('/');
+
+  if (formattedPath) {
+    fs.mkdirpSync(formattedPath);
+  }
+
+  fs.createFileSync(filePath);
+};
