e68ab435804b091482e6bad0e15367d608d20cae
Manogel <manoelgomes53@gmail.com>
2022-12-26 15:53:11 -0300

    fix: unit tests

--- src/stages/__tests__/genCoverageReportInMarkdown.spec.ts
+++ src/stages/__tests__/genCoverageReportInMarkdown.spec.ts
@@ -1,30 +1,27 @@
 import {
   mockResultsFromTextFile1,
   mockResultsFromTextFile2,
-} from '~/generators/__tests__/_mocks';
-import { genCoverageReportInMarkdown } from '~/generators/genCoverageReportInMarkdown';
+} from '~/stages/__tests__/_mocks';
+import { genCoverageReportInMarkdown } from '~/stages/genCoverageReportInMarkdown';
 
-const mockParseCoverageFromTextFileFn = jest.fn();
+// const mockParseCoverageFromTextFileFn = jest.fn();
 
-jest.mock('~/utils/parseCoverageFromTextFile', () => {
-  return {
-    parseCoverageFromTextFile: (...args: any) =>
-      mockParseCoverageFromTextFileFn(...args),
-  };
-});
+// jest.mock('~/utils/parseCoverageFromTextFile', () => {
+//   return {
+//     parseCoverageFromTextFile: (...args: any) =>
+//       mockParseCoverageFromTextFileFn(...args),
+//   };
+// });
 
 describe('genCoverageReportInMarkdown', () => {
-  const pathToFile = 'path-to-file';
   it('should be return coverage report #1', async () => {
-    mockParseCoverageFromTextFileFn.mockReturnValue(mockResultsFromTextFile1);
-    const results = genCoverageReportInMarkdown(pathToFile);
+    const results = genCoverageReportInMarkdown(mockResultsFromTextFile1);
     expect(results.indexOf('📦 All files')).toBeGreaterThan(-1);
     expect(results.indexOf('• src')).toBeGreaterThan(-1);
   });
 
   it('should be return coverage report #2', async () => {
-    mockParseCoverageFromTextFileFn.mockReturnValue(mockResultsFromTextFile2);
-    const results = genCoverageReportInMarkdown(pathToFile);
+    const results = genCoverageReportInMarkdown(mockResultsFromTextFile2);
     expect(results.indexOf('📦 All files')).toBeGreaterThan(-1);
     expect(results.indexOf('📌 index.ts')).toBeGreaterThan(-1);
   });
