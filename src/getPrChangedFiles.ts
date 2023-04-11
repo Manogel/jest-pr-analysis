5c62754e50fe4c87970cf72ed33bf57c935c9abd
Manogel <manoelgomes53@gmail.com>
2023-04-11 14:32:50 -0300

    feat: add parse-diff

--- src/getPrChangedFiles.ts
+++ src/getPrChangedFiles.ts
@@ -1,7 +1,4 @@
-import micromatch from 'micromatch';
-
 import { getPrDiffFiles } from '~/stages/getPrDiffFiles';
-import { getRelatedTestFiles } from '~/stages/getRelatedTestFiles';
 import { getJestParams } from '~/utils/getJestParams';
 
 const normalizeChangedFilesPath = (
@@ -25,28 +22,33 @@ export const getPrChangedFiles = async (actionParams: IActionParams) => {
   };
 
   const filesDiffList = await getPrDiffFiles(actionParams);
-  const filenamesList = filesDiffList.map(({ filename }) => filename);
-
-  // Filter files by collectCoverageFrom
-  const changedFilesArray = micromatch(
-    filenamesList,
-    jestParams.collectCoverageFrom,
-  );
-  prevResults.prChangedFiles = normalizeChangedFilesPath(
-    changedFilesArray,
-    jestParams.rootDir,
-  );
-
-  if (changedFilesArray.length <= 0) {
-    return prevResults;
-  }
-
-  // Get related test files (*.spec.ts, *.test.ts)
-  const filesToTestArray = await getRelatedTestFiles(
-    changedFilesArray,
-    jestParams.testRegex,
-  );
-  prevResults.filesToTest = filesToTestArray;
-
+  console.log(filesDiffList);
   return prevResults;
+  // const addedOrModified = filesDiffList.filter(
+  //   ({ status }) => status === 'added' || status === 'modified',
+  // )
+  // const filenamesList = filesDiffList.map(({ filename }) => filename);
+
+  // // Filter files by collectCoverageFrom
+  // const changedFilesArray = micromatch(
+  //   filenamesList,
+  //   jestParams.collectCoverageFrom,
+  // );
+  // prevResults.prChangedFiles = normalizeChangedFilesPath(
+  //   changedFilesArray,
+  //   jestParams.rootDir,
+  // );
+
+  // if (changedFilesArray.length <= 0) {
+  //   return prevResults;
+  // }
+
+  // // Get related test files (*.spec.ts, *.test.ts)
+  // const filesToTestArray = await getRelatedTestFiles(
+  //   changedFilesArray,
+  //   jestParams.testRegex,
+  // );
+  // prevResults.filesToTest = filesToTestArray;
+
+  // return prevResults;
 };
--- src/stages/getPrDiffFiles.ts
+++ src/stages/getPrDiffFiles.ts
@@ -1,6 +1,25 @@
 import { context, getOctokit } from '@actions/github';
+import parseDiff from 'parse-diff';
 
 export const getPrDiffFiles = async (actionParams: IActionParams) => {
+  const { owner, repo } = context.repo;
+  const octokit = getOctokit(actionParams.ghToken);
+
+  const response = await octokit.rest.pulls.get({
+    owner,
+    repo,
+    pull_number: actionParams.prNumber,
+    headers: {
+      accept: 'application/vnd.github.v3.patch',
+    },
+  });
+
+  const patch = parseDiff(response.data as unknown as string);
+
+  return patch;
+};
+
+export const getPrDiffFiles2 = async (actionParams: IActionParams) => {
   try {
     const { owner, repo } = context.repo;
 
@@ -11,7 +30,11 @@ export const getPrDiffFiles = async (actionParams: IActionParams) => {
       pull_number: actionParams.prNumber,
     });
 
-    return files;
+    const addedOrModified = files.filter(
+      ({ status }) => status === 'added' || status === 'modified',
+    );
+
+    return addedOrModified;
   } catch (err) {
     console.warn('Error to show files diff', err);
     throw err;
